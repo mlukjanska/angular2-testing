7e7dbb7951defa0580b941455e149f728cdf22b6
Margarita Lukjanska <mlukjanska@users.noreply.github.com>
2017-04-25 21:14:42 +0100

    Updated to player.spec - corrected 'should update currentlyPlayingSong and playing' test

--- jasmine/src/Player.spec.js
+++ jasmine/src/Player.spec.js
@@ -30,7 +30,7 @@ describe("Player", function() {
     it("should update currentlyPlayingSong and playing", function() {
       player.play(song);
       expect(player.currentlyPlayingSong).toEqual(song);
-      expect(player.playing).toBe(true);
+      expect(player.isPlaying).toBe(true);
     });
 
     // demonstrates custom matcher
@@ -42,7 +42,7 @@ describe("Player", function() {
 
   describe('makeFavorite()', function() {
     // demonstrates use of spies to intercept and test method calls
-    it("should update the song that it is a favorite", function() {
+    it("should update the song that it is a favorite (with spyOn)", function() {
         spyOn(song, 'persistFavoriteStatus'); 
         player.play(song);
         player.makeFavorite();
@@ -51,7 +51,12 @@ describe("Player", function() {
     });
 
     // same as above except with createSpyObj()
-    it("should update the song that it is a favorite", function() {
+    it("should update the song that it is a favorite (with createSpyObj())", function() {
+        var song = jasmine.createSpyObj('song', ['persistFavoriteStatus']); 
+        player.play(song);
+        player.makeFavorite();
+        
+        expect(player.currentlyPlayingSong.persistFavoriteStatus).toHaveBeenCalledWith(true);
     });
   });
 });
