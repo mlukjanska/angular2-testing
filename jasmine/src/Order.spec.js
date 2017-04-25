1b6b002c22b0adbb2b2ebc074f00696e877e6a85
Margarita Lukjanska <mlukjanska@users.noreply.github.com>
2017-04-25 21:17:32 +0100

    Step 3 finished - updated tests for Order

--- jasmine/src/Order.spec.js
+++ jasmine/src/Order.spec.js
@@ -8,9 +8,23 @@ describe('Order', function() {
 
   describe('addItem(name, cost)', function() {
     it('should not discount unpreferred customers', function() {
+        spyOn(customer, 'isPreferred'); 
+        customer.isPreferred.and.returnValue(false);
+        
+        order.addItem('NonDiscountCustomer', 10);
+        order.getTotal();
+        expect(order.getTotal()).toEqual(10);
+
     });
 
     it('should give preferred customers a 10% discount', function() {
+        spyOn(customer, 'isPreferred'); 
+        customer.isPreferred.and.returnValue(true);
+        
+        order.addItem('NonDiscountCustomer', 10);
+        order.getTotal();
+        expect(order.getTotal()).toEqual(9);
+
     });
   });
 });
